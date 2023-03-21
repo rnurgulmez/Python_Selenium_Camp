ad8fd25458bcde115e43ba17222fb54ed9189b01
Rabia Nur Gülmez <99583312+rabianurg8@users.noreply.github.com>
2023-03-21 10:22:34 +0100

    Add files via upload

---
+++ Homework/homework5.py
@@ -0,0 +1,24 @@
+### HTML ###
+
+# Tum websiteleri HTML kullanir cunku HTML web sayfalarinin iskeleti olarak dusunebiliriz. Resim,video,etiket,form gibi ogeleri HTML sayesinde web sitemizin yapisina ekleyebiliriy. HTML adi "hyper text markup language"den gelmektedir.
+# Bir HTML dosyası bir text dosyası olmakla beraber markup tags (işaretlenmiş etiketler) içerir
+# Markup tag'ler web tarayıcınıza sayfanın nasıl gösterileceğini gösterir.
+
+### HTML Locators ###
+
+# HTML Locators,web sitelerindeki elementlerle etkilesime gecmek icin kullandigimiz bir aractir. Ornegin googleda arama yapmak istiyorsak arama butonunu temsilen bir ID,NAME veya XPATH bulmak gibi etkilesime gecmememiz icin elzemdir.Html Locators, web scraping ve test otomasyonlari gibi islemlerde kullanilir
+
+# Locator cesitleri su sekildedir : ID,Name,ClassName,TagName,LinkText,CssSelector,XPath
+
+### Selenium'da Aksiyonlar ###
+
+# click() = Bir öğeye tıklamak için kullanılır..
+# clear() = Bir öğenin içeriğini temizlemek için kullanılır. 
+# submit() = Bir formu göndermek için kullanılır. 
+# select_by_visible_text() = Bir dropdown öğesinde belirli bir metne tıklamak için kullanılır.
+# scroll() = Sayfayı aşağı veya yukarı kaydırmak için kullanılır.
+# switch_to_Frame() = Bir iframe içine geçmek için kullanılır. 
+# switch to Window = Bir pencereye geçmek için kullanılır. 
+# Back() = Önceki sayfaya gitmek için kullanılır. 
+# Forward() = Sonraki sayfaya gitmek için kullanılır. 
+# Refresh() = Sayfayı yenilemek için kullanılır.
